5f40c693ab99e113249bd623cb1d834427cc4fcc
Andrew Nomura <drew97.nomura@gmail.com>
2019-11-15 20:18:56 -0800

    Update end_to_end.py

--- examples/end_to_end.py
+++ examples/end_to_end.py
@@ -3,12 +3,8 @@ import h5py
 from keras.layers import Dense
 from keras.models import Sequential
 
-from dlgo.agent.predict import DeepLearningAgent
-from dlgo.agent.predict import load_prediction_agent
-from dlgo.agent.termination import PassWhenOpponentPasses
 from dlgo.data.parallel_processor import GoDataProcessor
 from dlgo.encoders.sevenplane import SevenPlaneEncoder
-from dlgo.gtp.play_local import LocalGtpBot
 from dlgo.httpfrontend import get_web_app
 from dlgo.networks import large
 from dlgo.agent.predict import *
@@ -37,17 +33,11 @@ model.fit(X, y, batch_size=128, epochs=1, verbose=1)
 # end::e2e_model[]
 
 # tag::e2e_agent[]
-h5File = h5py.File("../agents/deep_bot.h5", "w")
 deep_learning_bot = DeepLearningAgent(model, encoder)
-print("test")
-deep_learning_bot.serialize(h5File)
-print("success")
+with h5py.File('../agents/deep_bot.h5', 'w') as f:
+    deep_learning_bot.serialize(f)
 # deep_learning_bot = load_prediction_agent(h5py.File("../agents/deep_bot.h5", "r"))
-gtp_bot = LocalGtpBot(go_bot=deep_learning_bot, termination=PassWhenOpponentPasses(),
-                      handicap=0, opponent='gnugo')
-# deep_learning_bot = DeepLearningAgent(model, encoder)
-# deep_learning_bot.serialize("../agents/deep_bot.h5")
-# gtp_bot.run()
+# bot = load_prediction_agent(h5py.File("../agents/deep_bot.h5", "r"))
 # end::e2e_agent[]
 
 # tag::e2e_load_agent[]
@@ -55,7 +45,5 @@ model_file = h5py.File("../agents/deep_bot.h5", "r")
 bot_from_file = load_prediction_agent(model_file)
 
 web_app = get_web_app({'predict': bot_from_file})
-print("testing web")
 web_app.run()
-print("success 2")
 # end::e2e_load_agent[]
